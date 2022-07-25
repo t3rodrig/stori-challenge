dc4e7c4ef52eb9a41f9f3e44e72b0f2c1e5ff778
Tonalli R.-L <t3rodrig@users.noreply.github.com>
2022-07-25 11:28:45 -0500

    Fix user model
    add the "new" keyword when creating a schema

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = Schema(
+const userSchema = new Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
