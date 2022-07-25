56d1a6ca513c2406d72a309f41413f009ba96738
Tonalli R.-L <t3rodrig@users.noreply.github.com>
2022-07-25 18:32:00 -0500

    Fix multer unexpected field
    The <NAME> you use in multer's upload.single(<NAME>) function
    must be the same as the one you use in <input type="file" name="<NAME>" ...>.

--- backend/routes/newsletters.js
+++ backend/routes/newsletters.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const Newsletter = require("../models/Newsletter");
 const parser = require("../configs/cloudinary");
 
-router.post('/create', parser.single("image") ,function(req, res, next) {
+router.post('/create', parser.single("file") ,function(req, res, next) {
     res.json(req.file);
 });
 
